54dc857c59b0a6dfc4a907b27e5bcedca9600356
naglec13 <naglec13@gmail.com>
2020-04-25 21:27:57 +0300

    ..interface for DB

--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -1,4 +1,9 @@
 package com.example.sweater.repos;
 
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
 public interface MessageRepo {
+    public interface UserRepository extends CrudRepository<Message, Integer> {
+    }
 }
