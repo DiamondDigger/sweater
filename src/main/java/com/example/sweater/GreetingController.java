2da022031b7cdcae8c0237008e87e60ac1835584
naglec13 <naglec13@gmail.com>
2020-04-27 23:27:06 +0300

    ..

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -15,6 +15,10 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
+    public GreetingController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
