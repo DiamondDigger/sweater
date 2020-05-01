805ca68b965459999d542398610f4b86701ae350
naglec13 <naglec13@gmail.com>
2020-05-02 01:19:58 +0300

    ..

---
+++ src/main/java/com/example/sweater/controllers/RegistratitonController.java
@@ -0,0 +1,24 @@
+package com.example.sweater.controllers;
+
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistratitonController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user){
+    return "redirect:/login";
+    }
+}
